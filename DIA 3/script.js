3676a034be771053b9cff79c70015b1bc34dbcbc
Rodrigo Peixoto <rodrigopeixoto2000@hotmail.com>
2024-05-16 20:59:53 -0300

    atualizando dia 3 com imc

--- DIA 3/script.js
+++ DIA 3/script.js
@@ -1,4 +1,22 @@
-let tipoNumber = 2
-let tipoString = "2"
-let tipoBooleantexto = "true"
-let tipoBoolean = "true"
+let nome = ""
+let idade = 0
+let altura = 0 
+let peso = 0 
+
+//
+nome = prompt("qual o seu nome?")
+idade = parseInt(prompt("digite sua idade"))
+altura = parseFloat(prompt("digite sua altura"))
+peso = parseInt(prompt("digite seu peso"))
+//
+
+let AnoDeNascimento = 0 
+AnoDeNascimento = 2024 - idade
+
+//
+let IMC = 0
+IMC = peso / (altura * altura)
+
+//
+
+console.log("Olá " + nome +", você tem" + idade +  " anos, nasceu em" + AnoDeNascimento + ", tem" + altura + ", pesa" + peso + "kg seu IMC é"+ IMC + "kg/m2")
